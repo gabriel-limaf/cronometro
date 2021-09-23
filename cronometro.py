ab95a7c7b515412aa841787103428199d5d56fdc
Gabriel Lima <gabriel.lim4f@gmail.com>
2021-09-23 12:04:43 -0300

    Add files via upload

---
+++ cronometro.py
@@ -0,0 +1,29 @@
+"""
+Cronometro by Gabriel Lima
+"""
+from time import sleep
+import pyttsx3
+
+print('Bem-vindo ao cronometro em Python!')
+
+
+def cronometro():
+    entrada_h = int(input('Defina as horas do temporizador: '))
+    entrada_m = int(input('Defina os minutos do temporizador: '))
+    entrada_s = int(input('Defina os segundos do temporizador: '))
+    timer = (entrada_h * 3600) + (entrada_m * 60) + entrada_s
+    for contagem in range(0, timer):
+        sleep(1)
+    esgotado('Tempo esgotado')
+    sleep(1)
+    esgotado('Tempo esgotado')
+    exit()
+
+
+def esgotado(texto):
+    engine = pyttsx3.init()
+    engine.say(texto)
+    engine.runAndWait()
+
+
+cronometro()
