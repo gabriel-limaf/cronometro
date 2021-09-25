ed5c0f915ff88595ffa2cbdaefd914d931f3cd5e
Gabriel Lima <gabriel.lim4f@gmail.com>
2021-09-25 10:37:00 -0300

    Update cronometro.py

--- cronometro.py
+++ cronometro.py
@@ -1,19 +1,36 @@
 """
-Cronometro by Gabriel Lima
+Temporizador by Gabriel Lima
 """
 from time import sleep
 import pyttsx3
+import os
 
-print('Bem-vindo ao cronometro em Python!')
+print('Bem-vindo ao temporizador em Python!')
 
 
-def cronometro():
-    entrada_h = int(input('Defina as horas do cronometro: '))
-    entrada_m = int(input('Defina os minutos do cronometro: '))
-    entrada_s = int(input('Defina os segundos do cronometro: '))
+def temporizador():
+    m = 0
+    h = 0
+    s = 0
+    entrada_h = int(input('Defina as horas do temporizador: '))
+    # h = entrada_h * 3600
+    entrada_m = int(input('Defina os minutos do temporizador: '))
+    # m = entrada_m * 60
+    entrada_s = int(input('Defina os segundos do temporizador: '))
     timer = (entrada_h * 3600) + (entrada_m * 60) + entrada_s
-    for contagem in range(0, timer):
+    for contagem in range(0, timer + 1):
+        if contagem // 60 == contagem / 60:
+            m = m + 1
+            s = 0
+        if contagem // 60 != contagem / 60:
+            s = s + 1
+        if contagem // 3600 == contagem / 3600:
+            h = h + 1
+        if contagem < 60:
+            s = contagem
+        print(f'{h - 1}:{m - 1}:{s}')
         sleep(1)
+        os.system("cls") or None
     esgotado('Tempo esgotado')
     sleep(1)
     esgotado('Tempo esgotado')
@@ -26,4 +43,4 @@ def esgotado(texto):
     engine.runAndWait()
 
 
-cronometro()
+temporizador()
